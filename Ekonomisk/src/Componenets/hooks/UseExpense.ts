00336b1a4f0a7e5ff7a370294e6f08bf5dcb21fe
melvinsjostrand <89245348+melvinsjostrand@users.noreply.github.com>
2024-09-29 17:24:57 +0200

    fix

--- Ekonomisk/src/Componenets/hooks/UseExpense.ts
+++ Ekonomisk/src/Componenets/hooks/UseExpense.ts
@@ -4,6 +4,7 @@ import apiClient from "./apiClient";
 
 
 export interface Expense {
+  income: number;
   category: string;
   amount: number;
   expenseId: number;
@@ -20,7 +21,7 @@ export interface ExpensesProps {
 
 const UseExpenses= () => {
   const fetchExpenses = () =>
-    apiClient.get<ExpensesProps>("API").then((res) => res.data);
+    apiClient.get<ExpensesProps>("/Expenses/IncomeExpenses?userId="+1).then((res) => res.data);
 
   return useQuery<ExpensesProps, Error>({
     queryKey: ["Expenses"],
